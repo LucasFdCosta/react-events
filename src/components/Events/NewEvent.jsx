5c0fcf14355a9921484bc56ff0bc00311d352363
Lucas Costa <lucas.co090869@gmail.com>
2025-07-23 20:47:32 -0300

    feat: implement event creation with useMutation and handle submission in NewEvent component

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -2,15 +2,25 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useMutation } from '@tanstack/react-query';
+import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent
+  });
+
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
+        {isPending && "Submiting..."}
         <>
           <Link to="../" className="button-text">
             Cancel
@@ -20,6 +30,12 @@ export default function NewEvent() {
           </button>
         </>
       </EventForm>
+      {isError && 
+        <ErrorBlock 
+          title={"Failed to create event"}
+          message={error.info?.message || "Failed to create event please check your inputs and try again later"}
+        />
+      }
     </Modal>
   );
 }
